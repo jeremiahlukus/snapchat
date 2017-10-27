ca973d809dec964fb5917d5081eb557efb95a93b
jeremiahlukus <jeremiahlukus1@gmail.com>
2017-10-27 09:06:34 -0400

    Updated Info.plist for image picker

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { View } from "react-native";
-import Home from "./components/layout/Home";
+import { Home } from "./components/layout";
 
 class App extends Component {
   render() {
